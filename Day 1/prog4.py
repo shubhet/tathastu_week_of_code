77504f91846705eb96d79816f1ed34279b8afa3e
shubhet <65854158+shubhet@users.noreply.github.com>
2020-05-28 23:26:10 +0530

    Create prog4.py

---
+++ Day 1/prog4.py
@@ -0,0 +1,6 @@
+cp=float(input("Enter cost price  of product :"))
+sp=float(input("Enter sell price of product : "))
+profit=sp-cp
+newsellprice=1.05 *profit + cp
+print("Profit from this sale ",profit)
+print("To increase the profit and price should be ",newsellprice)
