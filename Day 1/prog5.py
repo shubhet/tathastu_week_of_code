4b0c25e3e3d02aab7ea7780f102ecab015e6df2f
shubhet <65854158+shubhet@users.noreply.github.com>
2020-05-28 23:34:51 +0530

    Create prog5.py

---
+++ Day 1/prog5.py
@@ -0,0 +1,17 @@
+rp1=int(input("Enter the run scored by player 1 in 60 ball: "))
+rp2=int(input("Enter the run scored by player 2 in 60 ball: "))
+rp3=int(input("Enter the run scored by player 3 in 60 ball: "))
+sr1=rp1 * 100 / 60
+sr2=rp2 * 100 /60
+sr3=rp3 * 100 /60
+print("Strike rate of player1 :",sr1)
+print("Strike rate of player2 :",sr2)
+print("Strike rate of player3 :",sr3)
+print("Run scored by player1 if they played 60 more balls is :",rp1 * 2)
+print("Run scored by player2 if they played 60 more balls is :",rp2 * 2)
+print("Run scored by player3 if they played 60 more balls is :",rp3 * 2)
+print("Maximum number of sixes player1 could hit :",rp1 // 6)
+print("Maximum number of sixes player2 could hit :",rp2 // 6)
+print("Maximum number of sixes player3 could hit :",rp3 // 6)
+
+
