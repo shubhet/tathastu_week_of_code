e06f9d1224204537d52f8917d36055b05f0f4afb
shubhet <65854158+shubhet@users.noreply.github.com>
2020-05-29 20:30:28 +0530

    Create prog3.py

---
+++ Day 03/prog3.py
@@ -0,0 +1,11 @@
+def duplicate(string):
+
+    duplicateString = ""
+
+    for x in string:
+        if x not in duplicateString:
+            duplicateString += x
+    return duplicateString
+string = input("Enter the string: ")
+print("String after removing :", duplicate(string))
+
